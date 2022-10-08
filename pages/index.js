d4659cd6b0cd9e5e7ee84550efed29f016002464
joyjsmun <joy.js.mun@gmail.com>
2022-10-08 14:59:28 -0500

    ADD: creating first page

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,23 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>PULP</title>
+        <title>PULP / Lenders Platform</title>
       </Head>
+      <div className='flex w-[95vw]'>
+        <div className='flex' id="topMenu">
+          <div>Logo</div>
+          <div>PULP</div>
+        </div>
+        <div>
+          <div>Login</div>
+          <div>I am a Borrower</div>
+        </div>
+      </div>
       <div>
-        
+        <div className='text-[4vw] font-nato text-center '>
+        Private Undercollateralized Lending Protocol
+        </div>
+        <div></div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,29 @@ module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
+    
   ],
   theme: {
-    extend: {},
+    extend: {
+      cursor: {
+        
+      },
+      fontFamily: {
+        // 'sans': ['Proxima Nova', ...defaultTheme.fontFamily.sans],
+        'bebas': ['Bebas Neue', 'cursive'],
+        'nato':['Noto Sans JP', 'sans-serif'],
+        'ProtoMono-Light':['ProtoMono-Light', 'Helvetica', 'Arial', 'Sans-Serif'],
+        'ProtoMono-LightShadow':['ProtoMono-LightShadow', 'Helvetica', 'Arial', 'Sans-Serif'],
+        'ProtoMono-SemiBold':['ProtoMono-SemiBold', 'Helvetica', 'Arial', 'Sans-Serif'],
+        
+      },
+      keyframes: {
+       
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+   
+]
+,
 }
